f90814b19b6845672b4e53f55a252b5a21599345
Egeeoz <ozakdemirege@hotmail.com>
2023-12-26 13:33:38 +0100

    Implemented book data fetching from API and assignment to 'books' array using the Book interface.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  publisher: string;
+  year: number;
+  pages: number | null;
+  plot: string;
+  audience: string;
+  color: string;
+}
 
+const books: Book[] = [];
+
+async function getApiData(): Promise<void> {
+  try {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books",
+      {
+        method: "GET",
+      }
+    );
+
+    let data: Book[] = await response.json();
+    books.push(...data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getApiData().then(() => {
+  console.log(books);
+});
